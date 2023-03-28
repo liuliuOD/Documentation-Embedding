3ec5717850c4ae55169d582f1c2df97f54758eed
liuliu <liuliugit@gmail.com>
2023-03-29 00:30:47 +0800

    feat: add middleware logger to show request info in console

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware};
 
 mod routes;
 mod app_provider;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
   HttpServer::new(|| {
     App::new()
+      .wrap(middleware::Logger::default())
       .configure(app_provider::router)
   })
     .bind(("127.0.0.1", 8080))?
